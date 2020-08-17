67e743e60784a23f37e9f04946c9da1503efd6bb
David Layec <davidlayec@orange.fr>
2020-08-17 16:56:02 +0200

    validations booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :studio
+  validates :time_in, presence: true
+  validates :time_out, presence: true
+  validates :booked, presence: true
+  validates :value, presence: true
 end
