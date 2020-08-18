9f407219ac11006b8ecaf1dba6409f71324f24e6
Majid Fakhoury <majidfakhoury@gmail.com>
2020-08-18 12:53:08 +0300

    created first version of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :studios, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :users do
+    resources :studios, only: [:update, :edit, :destroy]
+    resources :bookings, only: [:show, :index]
+  end
+
+  resources :bookings, only: [:update, :edit, :destroy]
 end
