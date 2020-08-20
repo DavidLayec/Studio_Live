2f79600e6d20c9a3ac116560383e1bdbe9eb5936
Tom Renard <tom.renard@zoho.com>
2020-08-20 15:57:21 +0200

    pundit studio

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -2,23 +2,22 @@ class StudiosController < ApplicationController
   skip_before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @studios = Studio.geocoded.where.not(latitude: nil, longitude: nil)
+    @studios = policy_scope(Studio).geocoded.where.not(latitude: nil, longitude: nil)
     @markers = @studios.map do |studio|
-    { lat: studio.latitude,
-      lng: studio.longitude,
-      # infoWindow: render_to_string(partial: "/studios/info_window", locals: { studio: studio })
-    }
+      { lat: studio.latitude,
+        lng: studio.longitude }
+    end
   end
-end
 
-def new
-  @studio = Studio.new
-    # authorize @studio
+  def new
+    @studio = Studio.new
+    authorize @studio
   end
 
   def create
     @studio = Studio.create(studio_params)
     @studio.user = current_user
+    authorize @studio
     if @studio.save
       redirect_to studio_path(@studio)
     else
@@ -28,14 +27,15 @@ def new
 
   def show
     set_studio
-    # authorize @studio
+    @studio.user = current_user
+    authorize @studio
+  end
 
-    # @booking = Booking.new
-    # @booking.user = current_user
-    # @booking.studio = @studio
-    # why are the bookings here and not studios?
-
-    # authorize @booking
+  def destroy
+    set_studio
+    @studio.destroy
+    redirect_to root_path
+    authorize @studio
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :studios, only: [:index, :show, :new, :create] do
+  resources :studios, only: [:index, :show, :new, :create, :destroy] do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:index, :show, :update, :edit, :destroy]
+  resources :bookings, only: [:index, :show, :destroy]
 
   # pundit handles.. but focus on core user journey
   # resources :users do
